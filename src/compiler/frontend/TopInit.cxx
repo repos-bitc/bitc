aaa3f83449461144f99f3137bc105d5d3e039d53
Jonathan S. Shapiro <shap@eros-os.com>
2008-07-17 16:57:55 -0400

    Update many copyright notices.

--- src/compiler/frontend/TopInit.cxx
+++ src/compiler/frontend/TopInit.cxx
@@ -1,6 +1,6 @@
 /**************************************************************************
  *
- * Copyright (C) 2006, Johns Hopkins University.
+ * Copyright (C) 2008, Johns Hopkins University.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or
